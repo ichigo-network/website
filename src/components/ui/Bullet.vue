f8d1bc87f943c32c281928d67815bb742327bf10
Léonard Lecouey <leonard.lecouey@gmail.com>
2022-02-14 18:04:05 +0900

    style(component): add bullet

---
+++ src/components/ui/Bullet.vue
@@ -0,0 +1,39 @@
+<template>
+  <component
+    :is="tag"
+    class="inline-flex"
+  >
+    <div
+      :class="[
+        'h-2 w-2 rounded-full mt-2 mr-2 flex-shrink-0',
+        {
+          'bg-primary-300': color === 'primary',
+          'bg-secondary-300': color === 'secondary',
+        }
+      ]"
+    />
+    <div class="">
+      <slot />
+    </div>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      default: 'primary',
+      type: String,
+      validator: (value) => [
+        'primary',
+        'secondary',
+      ].includes(value),
+    },
+
+    tag: {
+      default: 'span',
+      type: String,
+    },
+  },
+};
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -36,5 +36,91 @@
         A new social ecosystem without gatekeepers or intermediaries where people can finally thrive in a fair digital world
       </p>
     </LayoutContainer>
+
+    <div class="h-px bg-gray-900" />
+    <div class="grid grid-cols-2">
+      <div class="p-10">
+        <div class="sticky top-0">
+          <UiHeading>
+            ichigo ecosystem
+          </UiHeading>
+
+          <p class="pt-10 text-gray-700">
+            ichigo network achieves to recreate social interactions ecosystem without the needs of a central authority and remove the powerful middle-men.
+            <br>
+            <br>
+            Using a layered architecture it proposes a fully decentralized approach and delegates control to its community and a network of trustless nodes.
+            <br>
+            <br>
+            It rewards all contributions to the ecosystem fairly and distributes financial incentives to every actors equitably,
+            including end-users, content creators, application providers and so on.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div
+          v-for="ecosystem, key in {
+            blockchain: {
+              label: 'networkSecurity',
+              items: [
+                'stacking',
+                'reputationSystem',
+                'collaterals',
+                'disputeResolving',
+              ]
+            },
+            layer2: {
+              label: 'ichigoNodes',
+              items: [
+                'User profiles',
+                'Social metadata system',
+                'Identification',
+                'Advertising marketplace',
+              ]
+            },
+            peerToPeerNetwork: {
+              label: 'flashStorage',
+              items: [
+                'decentralizedOnDemandStorageProvision',
+              ]
+            },
+            humanNetworkInterface: {
+              label: 'applicationLayer',
+              items: [
+                'encryption',
+                'presentation',
+                'dataFormat',
+              ]
+            },
+            humanBeing: {
+              label: 'userSpace',
+              items: [
+                'contentCreationAndEngagement',
+                'humanAuthorityEngagement',
+              ]
+            },
+          }
+          "
+          :key="key"
+          class="p-10 border-b last:border-b-0 border-l border-gray-900"
+        >
+          <h2 class="pb-4 text-xl">
+            <span class="text-primary-400">{{ key }}</span> <span class="text-gray-200">/</span> {{ ecosystem.label }}
+          </h2>
+
+          <div class="grid grid-cols-2 gap-2">
+            <UiBullet
+              v-for="item in ecosystem.items"
+              :key="item"
+              class="font-light"
+            >
+              {{ item }}
+            </UiBullet>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-px bg-gray-900" />
   </div>
 </template>
