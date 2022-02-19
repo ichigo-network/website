873c70383073e8eb4bc259edb70e11cd3240fd93
Léonard Lecouey <leonard.lecouey@gmail.com>
2022-02-19 15:36:29 +0900

    style: add title animation

--- src/components/ui/Divider.vue
+++ src/components/ui/Divider.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      ref="section"
+      ref="target"
       :class="[
         'h-px bg-gray-900 transition-transform duration-700 origin-left',
         inView ? 'scale-x-100' : 'scale-x-0',
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+let observer;
+
 export default {
   props: {
     once: {
@@ -28,14 +30,18 @@ export default {
   mounted() {
     if ('IntersectionObserver' in window) {
       const callback = (entries) => {
-        if (this.once && this.inView) return;
+        if (this.inView) return;
         this.inView = entries[0].isIntersecting;
       };
-      const observer = new IntersectionObserver(callback, {
-        threshold: [0.4],
-      });
-      observer.observe(this.$refs.section);
+      observer = new IntersectionObserver(callback);
+      observer.observe(this.$refs.target);
+    } else {
+      this.inView = true;
     }
+  },
+
+  beforeDestroy() {
+    if ('IntersectionObserver' in window) observer.disconnect();
   },
 };
 </script>
--- src/components/ui/Heading.vue
+++ src/components/ui/Heading.vue
@@ -1,6 +1,7 @@
 <template>
   <component
     :is="tag || `h${level}`"
+    ref="target"
     :class="[
       {
         'text-3xl sm:text-4xl md:text-5xl': (size || level) === 1,
@@ -9,12 +10,33 @@
         'text-lg sm:text-xl': (size || level) >= 4,
       },
     ]"
+    :aria-label="trimmedText"
   >
-    <slot />
+    <transition-group
+      v-if="splittedTex.length > 0"
+      enter-active-class="transition-opacity duration-500"
+      enter-class="opacity-0"
+      enter-to-class="opacity-100"
+    >
+      <template
+        v-for="word, key in splittedTex"
+      >
+        <span
+          v-if="inView"
+          :key="key"
+          aria-hidden="true"
+          :style="{ transitionDelay: `${key * 200}ms` }"
+        >{{ word }} </span>
+      </template>
+    </transition-group>
+
+    <slot v-else />
   </component>
 </template>
 
 <script>
+let observer;
+
 export default {
   props: {
     level: {
@@ -39,6 +61,39 @@ export default {
       default: null,
       type: String,
     },
+  },
+
+  data: () => ({
+    inView: false,
+  }),
+
+  computed: {
+    trimmedText() {
+      if (!this.$slots.default) return '';
+
+      return this.$slots.default[0].text.trim();
+    },
+
+    splittedTex() {
+      return this.trimmedText.split(' ');
+    },
+  },
+
+  mounted() {
+    if ('IntersectionObserver' in window) {
+      const callback = (entries) => {
+        if (this.inView) return;
+        this.inView = entries[0].isIntersecting;
+      };
+      observer = new IntersectionObserver(callback);
+      observer.observe(this.$refs.target);
+    } else {
+      this.inView = true;
+    }
+  },
+
+  beforeDestroy() {
+    if ('IntersectionObserver' in window) observer.disconnect();
   },
 };
 </script>
